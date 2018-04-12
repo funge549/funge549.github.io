0153f89f341d023552a5d84c79e3184a54596599
funge549 <evelin@newschool.edu>
2018-04-12 12:14:50 -0400

    General update of folder

---
+++ js-intro/main.js
@@ -0,0 +1,21 @@
+var myWeekend ='Eat ice cream';
+
+60 + 6 
+6 * 11
+70 - 4
+132/2 
+33 * 2
+67 - 1 
+
+var one =['Esra', 'Evelin', 'Prithi'];
+var two =['Emily', 'Emil' ,'Kristen'];
+var three = ['Yangchen', 'Yaren'];
+var four = ['Jahyun', 'Seowon', 'Soyeon'];
+var five = ['Vincent', 'Vinny'];
+var six = ['Khe', 'Shuting'];
+
+var allStudentsHere = true
+
+var studentsMissing = ['Nika','Kevin'];
+
+console.log(studentsMissing.length);
