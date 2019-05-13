b432662a9caae7162507d5145e58ffb7a67a9fb3
funge549 <evelin@newschool.edu>
2019-05-12 23:02:38 -0400

    Updtate Dat

--- datFeatures/featureSignatures/download/datSignatures.js
+++ datFeatures/featureSignatures/download/datSignatures.js
@@ -1,29 +1,35 @@
 function writePostandSignature(archive, postSubmission){
 	var archive = archive;
-	
+
 	postSubmission.addEventListener("submit", function(e) {
-  		e.preventDefault(); 
-	  		var formRecieved = e.target,
-			imageReader = new FileReader(),
-			formSignatureImage = ((formRecieved.getElementsByTagName("img").length > 0) ? formRecieved.getElementsByTagName
-			("img")[0].src : "not available" );
+	  	e.preventDefault(); 
+	  	var formRecieved = e.target,
+	  			formTitle = formRecieved.elements["Title"].value.toString(),
+	  			formContents = formRecieved.elements["Content"].value.toString(),
+	  			timestamp = new Date().getTime(),
+	  			imageReader = new FileReader();
 
-  		console.log("form recieved: ", formSignatureImage)
+		var formSignatureImage = ((formRecieved.getElementsByTagName("img").length > 0) ? formRecieved.getElementsByTagName("img")[0].src : "not available" );
+	  	
+	  	console.log("form recieved: ", formSignatureImage)
 
-	var postContent = {
-  		"signature" : formSignatureImage,
-  	}
+	  	var postContent = {
+	  		"title" : formTitle,
+	  		"timestamp" : timestamp,
+	  		"content" : formContents,
+	  		"signature" : formSignatureImage;
+	  	}
 
-	async function postFile(archive, postContent){
-		await archive.writeFile('/posts/post-' + postContent.timestamp +'.json', JSON.stringify(postContent));
-	}
+	  	async function postFile(archive, postContent){
+	  		await archive.writeFile('/posts/post-' + postContent.timestamp +'.json', JSON.stringify(postContent));
+	  	}
 
-  	postFile(archive, postContent)
-  	.then(function(event){
-  		console.log("post is posted!")
-  	})
-  	.catch(function(error){
-  		console.log("error\n", error)
+	  	postFile(archive, postContent)
+	  	.then(function(event){
+	  		console.log("post is posted!")
+	  	})
+	  	.catch(function(error){
+	  		console.log("error\n", error)
   		})
 	});
 }
