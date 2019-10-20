4a94947039e3d9cb4f517c2c6a742f5538fd4c12
funge549 <evelin@newschool.edu>
2019-10-20 15:27:56 -0400

    summer update

--- elastica/coffee-source/js/main.js
+++
@@ -1,8 +0,0 @@
-function getRandomInt(min, max) {
-	return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function buildColor( r, g, b ) {
-	return "rgb(" + r + ", " + g + ", " + b + ")";  
-}
-
